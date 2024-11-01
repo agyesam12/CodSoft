5a0da7100956ab8dd2811822dfb9702324f884e5
agyesam12 <agyesam12@gmail.com>
2024-11-01 17:33:37 +0000

    added a signin and signup view logics

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from .models import *
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView, ListView, DetailView, DeleteView, View, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.utils import timezone
+from django.urls import reverse
+from django.http import JsonResponse
+from .forms import *
+from .forms import RegisterUserForm,CreateTodoForm
+from .models import User
+
 
 # Create your views here.
 
@@ -7,10 +20,38 @@ def home(request):
     return render(request, 'home.html')
 
 def signin(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        phone_number = request.POST['phone_number']
+        password = request.POST['password']
+        user = authenticate(request, phone_number=phone_number, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                messages.success(request, "Logged in successfully")
+                return redirect('home')
+            else:
+                messages.warning(request, "Account is inactive")
+        else:
+            messages.info(request, "Account not found, please register")
     return render(request,'signin.html')
 
 def signup(request):
-    return render(request, 'signin.html')
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            messages.success(request, f"Acount created succesfully...")
+            return redirect("signin")
+        else:
+            print("Something went wrong")
+            messages.warning(request,"Something went wrong, try again")
+    else:
+        form = RegisterUserForm()
+    context = {'form':form}
+    return render(request, 'signin.html', context)
 
 def contact(request):
     return render(request, 'contact.html')
