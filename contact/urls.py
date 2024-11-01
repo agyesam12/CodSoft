1801914e18a23618eef33ed1ab37ee80182eb073
agyesam12 <agyesam12@gmail.com>
2024-11-01 16:21:30 +0000

    added contact,signup.login and about us urls paths

--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,10 @@ from . import views
 from .views import *
 
 urlpatterns = [
-    path('', )
+    path('',views.home, name='home'),
+    path('contact/',views.contact, name='contact'),
+    path('signin/',views.signin, name='signin'),
+    path('signup/',views.signup, name='signup'),
+    path('about/',views.about, name='about'),
+    #path('contact/',views.contact, name='contact'),
 ]
