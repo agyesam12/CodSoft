8d9dc148729bed7df2397fc08e681627bfe27050
agyesam12 <agyesam12@gmail.com>
2024-11-01 16:22:24 +0000

    added signup, signin, about us and contact home page view functions

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def signin(request):
+    return render(request,'signin.html')
+
+def signup(request):
+    return render(request, 'signin.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
