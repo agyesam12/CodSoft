c73ed4e1b8b1eee6b5391e5287802972f609a16f
agyesam12 <agyesam12@gmail.com>
2024-11-02 00:26:19 +0000

    added create contact class based function to the urls.py

--- contact/urls.py
+++ contact/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('signup/',views.signup, name='signup'),
     path('about/',views.about, name='about'),
     path('signout/',views.signout, name='signout'),
+    path('CreateContact/',CreateContact.as_view(), name='CreateContact'),
 ]
