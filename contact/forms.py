6d6e092c3c9ea26782d70a8e6164532c6252f55d
agyesam12 <agyesam12@gmail.com>
2024-11-13 13:37:30 +0000

    added has_whatsapp and save_to to forms for contact model

--- contact/forms.py
+++ contact/forms.py
@@ -22,22 +22,23 @@ class RegisterUserForm(UserCreationForm):
 class CreateContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ['first_name','last_name','location', 'email','phone_number','message']
+        fields = ['first_name', 'last_name', 'location', 'email', 'phone_number', 'message', 'save_to', 'has_whatsapp']
         widgets = {
-            
             'first_name': forms.TextInput(attrs={'placeholder': 'Please enter the firstname'}),
             'last_name': forms.TextInput(attrs={'placeholder': 'Please enter the last name'}),
             'location': forms.TextInput(attrs={'placeholder': 'Please enter the location'}),
             'email': forms.EmailInput(attrs={'placeholder': 'Please enter your email'}),
             'phone_number': forms.TextInput(attrs={'placeholder': 'Please enter the phone number'}),
-            'message': forms.Textarea(attrs={'place holder': 'please enter any message for this contact '})
+            'message': forms.Textarea(attrs={'placeholder': 'Please enter any message for this contact'}),
+            'save_to': forms.Select(attrs={'placeholder': 'Please select where to save'}),
+            'has_whatsapp': forms.CheckboxInput(attrs={'placeholder': 'Does this contact have WhatsApp?'})
         }
 
 
 class UpdateContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ['first_name','last_name','location', 'email','phone_number','message']
+        fields = ['first_name','last_name','location', 'email','phone_number','message','save_to','has_whatsapp']
 
 
 
